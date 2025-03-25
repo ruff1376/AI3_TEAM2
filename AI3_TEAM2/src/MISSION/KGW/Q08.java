4368ace9e2d8e78b41d6c10cb795a14a4fa95706
tj-bu-706-01 <tj-bu-706-01@DESKTOP-SMVSKUH>
2025-03-25 17:46:22 +0900

    2차원배열로 풀어야합니다

---
+++ AI3_TEAM2/src/MISSION/KGW/Q08.java
@@ -0,0 +1,34 @@
+package MISSION.KGW;
+
+import java.util.Scanner;
+
+public class Q08 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int count = 1;
+		int arr[][] = new int [n][n];
+		for (int i = 0; i < arr.length; i++) {
+			if (i%2 == 0) {
+				for (int j = 0; j < arr.length; j++) {
+					arr[i][j] = count;
+					count++;
+				}
+			}
+			if (i%2 == 1) {
+				for (int j = n-1; j >= 0; j--) {
+					arr[i][j] = count;
+					count++;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
